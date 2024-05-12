7013b420f31e89fbe6a0426b17e2aea6efa010bb
jameel raza <166707907+jameelraz@users.noreply.github.com>
2024-05-12 06:34:03 -0700

    Advance Todo List
    a cli base advance todo list created using typescript nodejs and inquirer

---
+++ index.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todoquestions = await inquirer.prompt([
+        { name: 'todo',
+            type: "input",
+            message: "what would you like to add in your todos",
+        },
+        { name: "addmore",
+            type: "confirm",
+            message: "do you want to add more in your todos",
+            default: "false",
+        },
+    ]);
+    todos.push(todoquestions.todo);
+    condition = (todoquestions.addmore);
+    console.log(todos);
+}
